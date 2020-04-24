dbac2369ab2e1b9ef6031d4eebaf3ca178c3f01a
Yilun-Sun <osirisnero@gmail.com>
2020-04-23 21:01:44 -0400

    update

--- src/Canvas/CanvasGraph.js
+++ src/Canvas/CanvasGraph.js
@@ -28,15 +28,14 @@ function drawGraph(graph) {
     drawBackground();
 
     let vertexNums = graph.length;
-    const rds = 200;
 
     drawFilledCircle({ ctx: ctx, x: 400 + rds * Math.cos(Math.PI / 2), y: 300 - rds * Math.sin(Math.PI / 2), radius: 20, color: "#2B5F75" });
 
     ctx.lineWidth = 3;  //设置线条宽度
+    console.log(cliqueNode)
     // draw lines
     for (let i = 0; i < vertexNums; i++) {
         for (let j = 1; j < graph[i].length; j++) {
-
 
             const temp_x1 = 400 + rds * Math.cos(Math.PI / 2 - (graph[i][0] - 1) * (Math.PI * 2) / vertexNums);
             const temp_y1 = 300 - rds * Math.sin(Math.PI / 2 - (graph[i][0] - 1) * (Math.PI * 2) / vertexNums);
@@ -45,8 +44,20 @@ function drawGraph(graph) {
 
             ctx.moveTo(temp_x1, temp_y1);
             ctx.lineTo(temp_x2, temp_y2);
+
+            // if (cliqueNode.includes(graph[i][0] - 1) && cliqueNode.includes(graph[i][j] - 1)) {
+            //     ctx.lineWidth = 7;  //设置线条宽度
+            //     ctx.strokeStyle = 'green';
+            //     console.log((graph[i][0] - 1) + ' ' + (graph[i][j] - 1))
+            // }
+            // else {
+            //     ctx.strokeStyle = 'black';
+            // }
+
             ctx.strokeStyle = 'black';   //绘制边框样式
             ctx.stroke();
+
+            ctx.lineWidth = 3;  //设置线条宽度
         }
     }
 
@@ -54,7 +65,12 @@ function drawGraph(graph) {
     for (let i = 0; i < vertexNums; i++) {
         const temp_x = 400 + rds * Math.cos(Math.PI / 2 - i * (Math.PI * 2) / vertexNums);
         const temp_y = 300 - rds * Math.sin(Math.PI / 2 - i * (Math.PI * 2) / vertexNums);
-        drawFilledCircle({ ctx: ctx, x: temp_x, y: temp_y, radius: 20, color: "#2B5F75" });
+        if (cliqueNode.includes(i)) {
+            drawFilledCircle({ ctx: ctx, x: temp_x, y: temp_y, radius: 20, color: "#7BA23F" });
+        }
+        else {
+            drawFilledCircle({ ctx: ctx, x: temp_x, y: temp_y, radius: 20, color: "#2B5F75" });
+        }
 
         ctx.font = "20px Georgia";
         ctx.fillStyle = "#FFFFFF";
@@ -62,6 +78,9 @@ function drawGraph(graph) {
     }
 
     // setCMDTextArea();
+
+    // reset 
+    cliqueNode = [];
 
 }
 
@@ -79,6 +98,7 @@ function setCMDTextArea() {
 
 
 }
+
 
 function getCMDTextArea() {
     let text = document.getElementById("cmdTextArea").value;
@@ -139,10 +159,31 @@ function drawBackground() {
 let canvasWidth = 800;
 let canvasHeight = 600;
 
+let prevX = 0;
+let prevY = 0;
+
+// let saveButton;
+// let clearButton;
+// let slider;
+// let xoff = 0;
+
+var canvasElementOffsetLeft;
+var canvasElementOffsetTop;
+
 var graph = [[1, 5, 2, 7, 8], [2, 4, 1, 3, 5, 6], [3, 2, 4, 5], [4, 2, 3, 5], [5, 2, 4, 1, 3], [6, 2], [7, 1], [8, 1]];
+var graph_1 = [[1, 5, 2, 7, 8, 9], [2, 4, 1, 3, 5, 6], [3, 2, 4, 5, 9], [4, 2, 3, 5], [5, 2, 4, 1, 3], [6, 2], [7, 1], [8, 1], [9, 1, 3]];
+var graph_2 = [[1, 5, 2], [2, 4, 1, 3, 5, 6], [3, 2, 4, 5], [4, 2, 3, 5], [5, 2, 4, 1, 3], [6, 2]];
+var graph_3 = [[1, 5, 2, 7], [2, 1, 3, 5, 6], [3, 2, 4, 5], [4, 3, 5], [5, 2, 4, 1, 3], [6, 2], [7, 1]];
+var graph_4 = [[1, 5, 2, 7, 8], [2, 1, 3, 5, 6], [3, 2, 4, 5], [4, 3, 5], [5, 2, 4, 1, 3], [6, 2], [7, 1, 8], [8, 1, 7]];
+
+const rds = 200;
 
 // const NODENUM = '4';
 // const TESTINPUT = '0 1\n' + '';
+var cliqueNode = [];
+
+let startNodeIndex = 0;
+let endNodeIndex = 0;
 
 function findMaximumClique() {
     console.log(graph);
@@ -208,23 +249,190 @@ function findMaximumClique() {
 
     // output result
     let temp = [];
+    let nodeResult = '';
     for (let index = 0; index < result.length; index++) {
         if ((result[index].match(/1/g) || []).length === bound) {
             temp.push(result[index]);
         }
     }
+
+
     result = temp;
-    document.getElementById("outputTextArea").value = ('result: ' + result);
+
+
+    for (let index = 0; index < result[0].length; index++) {
+        if (result[0].substring(index, index + 1) === '1') {
+            cliqueNode.push(index);
+        }
+    }
+    for (let index = 0; index < cliqueNode.length; index++) {
+        nodeResult += (cliqueNode[index] + 1) + ' ';
+    }
+
+    document.getElementById("outputTextArea").value = ('result: ' + result + '\nvertices in clique: ' + nodeResult);
 
     console.log('calculation times: ' + calculationTimes);
+
+    drawGraph(graph);
 }
 
+function updateNode() {
+    getCMDTextArea();
+    checktCMDTextArea();
+    document.getElementById("outputTextArea").value = '';
+}
 
+function findMaxClique() {
+    findMaximumClique();
+}
+
+function getNodeIndex(prevX, prevY) {
+    // console.log(prevX + ' ' + prevY);
+    let result = 0;
+    for (let i = 0; i < graph.length; i++) {
+        const temp_x = 400 + rds * Math.cos(Math.PI / 2 - i * (Math.PI * 2) / graph.length);
+        const temp_y = 300 - rds * Math.sin(Math.PI / 2 - i * (Math.PI * 2) / graph.length);
+        if ((temp_x - prevX) * (temp_x - prevX) + (temp_y - prevY) * (temp_y - prevY) < 400) {
+            // console.log('node:' + (i + 1));
+            result = i + 1;
+        }
+    }
+    return result;
+}
+
+function addOrRemoveEdge(startNodeIndex, endNodeIndex) {
+    let isAdd = true;
+
+    for (let i = 0; i < graph.length; i++) {
+        if (graph[i][0] === startNodeIndex) {
+            // check if need to remove
+            if (graph[i].includes(endNodeIndex)) {
+                isAdd = false;
+                break;
+            }
+        }
+        if (graph[i][0] === endNodeIndex) {
+            // check if need to remove
+            if (graph[i].includes(startNodeIndex)) {
+                isAdd = false;
+                break;
+            }
+        }
+    }
+
+    // add or remove
+    if (isAdd) {
+        for (let i = 0; i < graph.length; i++) {
+            if (graph[i][0] === startNodeIndex) {
+                // add
+                if (!graph[i].includes(endNodeIndex)) {
+                    graph[i].push(endNodeIndex);
+                }
+            }
+
+            if (graph[i][0] === endNodeIndex) {
+                // add
+                if (!graph[i].includes(startNodeIndex)) {
+                    graph[i].push(startNodeIndex);
+                }
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < graph.length; i++) {
+            if (graph[i][0] === startNodeIndex) {
+                // remove
+                if (graph[i].includes(endNodeIndex)) {
+                    console.log('removing ' + endNodeIndex + ' from ' + startNodeIndex);
+                    graph[i].splice(graph[i].indexOf(endNodeIndex), 1);
+                }
+            }
+
+            if (graph[i][0] === endNodeIndex) {
+                // remove
+                if (graph[i].includes(startNodeIndex)) {
+                    console.log('removing ' + startNodeIndex + ' from ' + endNodeIndex);
+                    graph[i].splice(graph[i].indexOf(startNodeIndex), 1);
+                }
+            }
+        }
+    }
+
+
+
+    // for (let i = 0; i < graph.length; i++) {
+    //     if (graph[i][0] === startNodeIndex) {
+    //         // add
+    //         if (!graph[i].includes(endNodeIndex)) {
+    //             graph[i].push(endNodeIndex);
+    //         }
+    //         // remove
+    //         else {
+    //             console.log('removing ' + endNodeIndex + ' from ' + startNodeIndex);
+    //             graph[i].splice(graph[i].indexOf(endNodeIndex), 1);
+    //             for (let j = 0; j < graph.length; j++) {
+    //                 if (graph[j][0] === endNodeIndex && graph[j].includes(startNodeIndex)) {
+    //                     graph[j].splice(graph[j].indexOf(startNodeIndex), 1);
+
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     if (graph[i][0] === endNodeIndex) {
+    //         // add
+    //         if (!graph[i].includes(startNodeIndex)) {
+    //             graph[i].push(startNodeIndex);
+    //         }
+    //         // remove
+    //         else {
+    //             console.log('removing ' + startNodeIndex + ' from ' + endNodeIndex);
+    //             graph[i].splice(graph[i].indexOf(startNodeIndex), 1);
+    //             console.log(graph[i])
+    //             for (let j = 0; j < graph.length; j++) {
+    //                 if (graph[j][0] === startNodeIndex && graph[j].includes(endNodeIndex)) {
+    //                     graph[j].splice(graph[j].indexOf(endNodeIndex), 1);
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    setCMDTextArea();
+    updateNode();
+}
+
+function graphExample1() {
+    graph = graph_1.slice();
+    drawGraph(graph);
+    setCMDTextArea();
+}
+
+function graphExample2() {
+    graph = graph_2.slice();
+    drawGraph(graph);
+    setCMDTextArea();
+}
+
+function graphExample3() {
+    graph = graph_3.slice();
+    drawGraph(graph);
+    setCMDTextArea();
+}
+
+function graphExample4() {
+    graph = graph_4.slice();
+    drawGraph(graph);
+    setCMDTextArea();
+}
 
 class CanvasGraph extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        let isMouseDown = false;
     }
 
     componentDidMount() {
@@ -251,17 +459,69 @@ class CanvasGraph extends React.Component {
         }
 
         drawGraph(graph);
-        
+
     }
 
     updateNode() {
         getCMDTextArea();
         checktCMDTextArea();
-        
+        document.getElementById("outputTextArea").value = '';
     }
 
     findMaxClique() {
         findMaximumClique();
+    }
+
+    handleMouseDown = (event) => {
+        console.log('mouse down');
+
+        this.isMouseDown = true;
+
+        // if window is resized
+        var canvasElement = document.getElementById("canvas");
+        canvasElementOffsetLeft = canvasElement.offsetLeft;
+        canvasElementOffsetTop = canvasElement.offsetTop;
+
+        // prevX = event.pageX - canvasElementOffsetLeft - canvasWidth / 2;
+        // prevY = event.pageY - canvasElementOffsetTop - canvasHeight / 2;
+        prevX = event.pageX - canvasElementOffsetLeft;
+        prevY = event.pageY - canvasElementOffsetTop;
+
+        startNodeIndex = getNodeIndex(prevX, prevY);
+        console.log('start at node-' + startNodeIndex);
+    }
+
+    handleMouseUp = (event) => {
+        console.log('mouse up');
+
+        this.isMouseDown = false;
+
+        endNodeIndex = getNodeIndex(prevX, prevY);
+        console.log('end at node-' + endNodeIndex);
+
+        if (startNodeIndex > 0 && startNodeIndex < graph.length + 1 && endNodeIndex > 0 && endNodeIndex < graph.length + 1) {
+            console.log('add link ' + startNodeIndex + ' ' + endNodeIndex);
+            addOrRemoveEdge(startNodeIndex, endNodeIndex);
+        }
+
+        startNodeIndex = 0;
+        endNodeIndex = 0;
+    }
+
+    handleMouseMove = (event) => {
+        var x = event.pageX - canvasElementOffsetLeft;
+        var y = event.pageY - canvasElementOffsetTop;
+
+        if (x > canvasWidth - 10 || x < 10 || y > canvasHeight - 10 || y < 10) {
+            this.isMouseDown = false;
+        }
+
+        const ctx = this.refs.canvas.getContext('2d');
+        if (this.isMouseDown) {
+
+            prevX = x;
+            prevY = y;
+        }
     }
 
     render() {
@@ -279,7 +539,7 @@ class CanvasGraph extends React.Component {
             // borderRadius: '5px',
             // backgroundColor: '#FFFFFF',
             width: '400px',
-            height: '150px',
+            height: '120px',
             fontSize: '20px'
         };
 
@@ -288,21 +548,22 @@ class CanvasGraph extends React.Component {
             // borderRadius: '5px',
             // backgroundColor: '#FFFFFF',
             width: '400px',
-            height: '50px',
+            height: '70px',
             fontSize: '20px'
         };
 
         return (
             <React.Fragment>
-                <canvas id='canvas' ref="canvas" width={canvasWidth} height={canvasHeight} />
-                <textarea id='cmdTextArea' style={textAreaStyle_1}></textarea>
-                <textarea id='outputTextArea' style={textAreaStyle_2}></textarea>
-                <button onClick={this.addNode} className="btn btn--stripe" style={clearButtonStyle}>Add New Node</button>
-                <button onClick={this.updateNode} className="btn btn--stripe" style={clearButtonStyle}>Update Node</button>
-                <button onClick={this.findMaxClique} className="btn btn--stripe" style={clearButtonStyle}>Find Max Clique</button>
+                <canvas id='canvas' ref="canvas" width={canvasWidth} height={canvasHeight} onMouseUp={this.handleMouseUp} onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} />
+                {/* <textarea id='cmdTextArea' style={textAreaStyle_1}></textarea>
+                <textarea id='outputTextArea' style={textAreaStyle_2}></textarea> */}
+                {/* <button onClick={this.addNode} className="btn btn--stripe" style={clearButtonStyle}>Add New Node</button> */}
+                {/* <button onClick={updateNode} className="btn btn--stripe" style={clearButtonStyle}>Update Node</button>
+                <button onClick={this.findMaxClique} className="btn btn--stripe" style={clearButtonStyle}>Find Max Clique</button> */}
             </React.Fragment>
         );
     }
 }
 
 export default CanvasGraph;
+export { updateNode, findMaxClique, graphExample1, graphExample2, graphExample3, graphExample4 };
--- src/Visualgo/Visualgo.js
+++ src/Visualgo/Visualgo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-import { Layout, Menu, Breadcrumb} from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './Visualgo.css';
-import logo from '../logo.svg';
+import logo from '../pixelLogo.svg';
+import logoPng from '../pixil-frame.png';
 import Canvas from '../Canvas/Canvas';
 import CanvasGraph from '../Canvas/CanvasGraph';
-import {selectSortAlgo} from '../Canvas/Canvas';
+import { selectSortAlgo } from '../Canvas/Canvas';
+
+import { updateNode, findMaxClique, graphExample1, graphExample2, graphExample3, graphExample4 } from '../Canvas/CanvasGraph';
 
 import ButtonGroup from 'antd/lib/button/button-group';
 
@@ -25,60 +28,108 @@ class Visualgo extends React.Component {
 
 
     render() {
+        const textAreaStyle_1 = {
+            // border: 0,
+            // borderRadius: '5px',
+            // backgroundColor: '#FFFFFF',
+            width: '400px',
+            height: '120px',
+            fontSize: '20px'
+        };
+
+        const textAreaStyle_2 = {
+            // border: 0,
+            // borderRadius: '5px',
+            // backgroundColor: '#FFFFFF',
+            width: '400px',
+            height: '120px',
+            fontSize: '20px'
+        };
+
+        const textStyle_1 = {
+            // border: 0,
+            // borderRadius: '5px',
+            // backgroundColor: '#FFFFFF',
+            color: 'white',
+            fontSize: '30px'
+        };
+
+        const textStyle_2 = {
+            // border: 0,
+            // borderRadius: '5px',
+            // backgroundColor: '#FFFFFF',
+            fontSize: '20px'
+        };
+
+        const textStyle_3 = {
+            // border: 0,
+            // borderRadius: '5px',
+            // backgroundColor: '#FFFFFF',
+            fontSize: '15px'
+        };
+
+        const divStyle_1 = {
+            float: 'right'
+        };
+
         return (
             <Layout style={{ minHeight: '100vh' }}>
                 <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                     <div >
-                        <img style={{ width: '85px' }} src={logo} alt="logo" />
+                        <img style={{ width: '85px' }} src={logoPng} alt="logo" />
                     </div>
                     <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                         <Menu.Item key="1">
                             {/* <Icon type="pie-chart" /> */}
-                            <span>Option 1</span>
+                            <span>Branch and Bound</span>
                         </Menu.Item>
                         <Menu.Item key="2">
                             {/* <Icon type="desktop" /> */}
-                            <span>Option 2</span>
+                            <span>Bron–Kerbosch</span>
                         </Menu.Item>
                         <SubMenu
                             key="sub1"
                             title={
                                 <span>
                                     {/* <Icon type="user" /> */}
-                                    <span>User</span>
+                                    <span>Node</span>
                                 </span>
                             }
                         >
-                            <Menu.Item key="3">Tom</Menu.Item>
-                            <Menu.Item key="4">Bill</Menu.Item>
-                            <Menu.Item key="5">Alex</Menu.Item>
+                            <Menu.Item key="3">Add Node</Menu.Item>
+                            <Menu.Item key="4">Remove Node</Menu.Item>
+                            <Menu.Item key="5">Show Neighbours</Menu.Item>
                         </SubMenu>
                         <SubMenu
                             key="sub2"
                             title={
                                 <span>
                                     {/* <Icon type="team" /> */}
-                                    <span>Team</span>
+                                    <span>Graph Examples</span>
                                 </span>
                             }
                         >
-                            <Menu.Item key="6">Team 1</Menu.Item>
-                            <Menu.Item key="8">Team 2</Menu.Item>
+                            <Menu.Item key="6" onClick={graphExample1}>Graph Example 1</Menu.Item>
+                            <Menu.Item key="7" onClick={graphExample2}>Graph Example 2</Menu.Item>
+                            <Menu.Item key="8" onClick={graphExample3}>Graph Example 3</Menu.Item>
+                            <Menu.Item key="9" onClick={graphExample4}>Graph Example 4</Menu.Item>
                         </SubMenu>
-                        <Menu.Item key="9">
+                        <Menu.Item key="10" onClick={findMaxClique}>
                             {/* <Icon type="file" /> */}
-                            <span>File</span>
+                            <span>Find Max Clique</span>
                         </Menu.Item>
-                        <Menu.Item key="10">
+                        <Menu.Item key="11" onClick={updateNode}>
                             {/* <Icon type="file" /> */}
-                            <button onClick={selectSortAlgo('bubble')}>Bubble</button>
+                            <span>Update Node</span>
+                            {/* <button >--Bubble--</button> */}
                         </Menu.Item>
                     </Menu>
                 </Sider>
                 <Layout>
                     <Header className="header">
                         <div className="logo" />
-                        <Menu
+                        <div style={ textStyle_1 }>Graph Algorithms Visualization</div>
+                        {/* <Menu
                             theme="dark"
                             mode="horizontal"
                             defaultSelectedKeys={['2']}
@@ -87,15 +138,29 @@ class Visualgo extends React.Component {
                             <Menu.Item key="1">nav 1</Menu.Item>
                             <Menu.Item key="2">nav 2</Menu.Item>
                             <Menu.Item key="3">nav 3</Menu.Item>
-                        </Menu>
+                        </Menu> */}
                     </Header>
                     <Content style={{ margin: '0 16px' }}>
                         <Breadcrumb style={{ margin: '16px 0' }}>
-                            <Breadcrumb.Item>Sorting</Breadcrumb.Item>
+                            <Breadcrumb.Item style={ textStyle_2 }>Graph Theory - Maximum Clique</Breadcrumb.Item>
                             {/* <Breadcrumb.Item>Bubble Sort</Breadcrumb.Item> */}
                         </Breadcrumb>
                         {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div> */}
                         <CanvasGraph />
+                        <div style={ divStyle_1 }>
+                            <div><label for="cmdTextArea" style={ textStyle_2 }>Edges: </label>
+                                <br />
+                                <textarea id="cmdTextArea" style={textAreaStyle_2}></textarea>
+                            </div>
+
+                            <div><label for="outputTextArea" style={ textStyle_2 }>Result Output: </label>
+                                <br />
+                                <textarea id='outputTextArea' style={textAreaStyle_2}></textarea>
+                            </div>
+                        </div>
+
+
+
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>Elonn Software ©2020 Created by Yilun Sun</Footer>
                 </Layout>
